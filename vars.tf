c40f025c96c5559e53d97c5e1e1e055fdc82cc9b
Matt McFarland <783024+vanetix@users.noreply.github.com>
2018-10-31 11:23:24 -0500

    Update source with terraform fmt run

--- vars.tf
+++ vars.tf
@@ -1,10 +1,9 @@
-
 variable "aws_profile" {
-  type = "string"
+  type        = "string"
   description = "Profile that your AWS provider will use to create resources"
 }
 
 variable "ami_id" {
-  type = "string"
+  type        = "string"
   description = "AMI ID for EC2 instance"
 }
